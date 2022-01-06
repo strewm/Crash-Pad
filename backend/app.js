6d0235d75a3769b9aad0bd6f1521c08a9a36ac24
Savanah Trewman <strewman@me.com>
2022-01-06 17:20:09 -0600

    user auth: added User model methods

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json()); // Middleware for parsing JSON bodies of reqs with Cont
 
 
 
-// Security Middleware (pre-request)
+// ------------------- Security Middleware (pre-request) ------------------- //
 if (!isProduction) {
     // Enable cors only in development! (React frontend served from different server than that of Express)
     app.use(cors());
@@ -41,10 +41,13 @@ app.use(
     })
 );
 
-// Route connections (connects all routes)
+
+// ------------------- Route connections (connects all routes) ------------------- //
 app.use(routes);
 
-// Resource not found error-handler middleware
+
+// ------------------- Error-handler middleware ------------------- //
+// Resource not found error-handler
 app.use((_req, _res, next) => {
     const err = new Error("The requested resource couldn't be found.");
     err.title = 'Resource not found.';
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,7 +1,60 @@
 'use strict';
 
 const { Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
+
+// ------------------- Instance model methods ------------------- //
+// Returns an object with only the User instance info that is safe to save to a JWT
+User.prototype.toSafeObject = function() {
+  const { id, username, email } = this; // context will be the User instance
+  return { id, username, email };
+};
+
+// Validate that 'password' string matches the User instance's hashedPassword
+User.prototype.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.hashedPassword.toString());
+};
+
+
+// ------------------- Static model methods ------------------- //
+// Returns a User with the requested id via currentUser scope
+User.getCurrentUserById = async function (id) {
+  return await User.scope('currentUser').findByPk(id);
+};
+
+// Search for one User with specified credential (username or email), then validate the password
+User.login = async function ({ credential, password }) {
+  const { Op} = require('sequelize');
+
+  const user = await User.scope('loginUser').findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+
+  if (user && user.validatePassword(password)) {
+    return await User.scope('currentUser').findByPk(user.id);
+  }
+};
+
+// During sign up, hash the User's password + return the created user
+User.signup = async function ({ username, email, password }) {
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    username,
+    email,
+    hashedPassword,
+  });
+
+  return await User.scope('currentUser').findByPk(user.id);
+};
+
+
+// ------------------- User Model ------------------- //
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
