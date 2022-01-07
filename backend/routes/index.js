2f74d3368b355aa31d15c0e12c4891a27b98ba91
Savanah Trewman <strewman@me.com>
2022-01-06 19:42:45 -0600

    double-checking git path OK after folder move

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@ const apiRouter = require('./api');
 // All of the URLs of the routes in the api router will have the prefix /api
 router.use('/api', apiRouter);
 
-// Test route
+// Test route (can remove this code)
 router.get('/hello/world', function(req, res) {
     // Setting a cookie on the response (name of 'XSRF-TOKEN') to the value of the req.csrfToken method's return
     res.cookie('XSRF-TOKEN', req.csrfToken());
