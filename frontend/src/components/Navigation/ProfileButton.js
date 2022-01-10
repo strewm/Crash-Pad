69573106baf22a5395d6f0ce908babc839c0b1f4
Savanah Trewman <strewman@me.com>
2022-01-09 21:14:30 -0600

    added more styling to splash page + fixed console error

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -31,7 +31,7 @@ function ProfileButton({ user }) {
     return (
         <>
             <button onClick={openMenu} id='profile-button'>
-                <i class="fas fa-user-circle"></i>
+                <i className="fas fa-user-circle"></i>
             </button>
             {showMenu && (
                 <ul className="profile-dropdown">
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -49,20 +49,71 @@ function SplashPage() {
             </form> */}
             <div className='main-header'>
                 <div id='main-header-title'>Find your basecamp.</div>
-                <div id='main-header-text'>Discover spots to stay by your favorite crags. Alpine starts supported by climbers, for climbers.</div>
+                <div id='main-header-text'>Discover spots to stay by your favorite crags.<br></br>Alpine starts supported by climbers, for climbers.</div>
             </div>
             <div className='main-container'>
                 <div>container 1</div>
                 <img src='/images/splash/splash-main-3.jpg' alt='splash main image'/>
             </div>
             <div className='sub-container-one'>
-                <div>container 2</div>
+                <div id='sub-tile-one'>
+                    <img src='/images/splash/splash-sub-one-1.jpg' alt='splash sub image'/>
+                    <div>Camping access made easier.</div>
+                </div>
+                <div id='sub-tile-two'>
+                    <img src='/images/splash/splash-sub-one-2.jpg' alt='splash sub image'/>
+                    <div>Climbing is our #1 priority.</div>
+                </div>
             </div>
             <div className='sub-container-two'>
-                <div>container 3</div>
+                <div id='sub-tile-three'>
+                    <img src='/images/splash/splash-sub-two-3.jpg' alt='splash sub image'/>
+                    <div>
+                        <div id='sub-tile-3-text'>Own land? Help enable access to climbing by providing campsites.</div>
+                        <div id='sub-tile-3-sub-text'>Host our community of adventure-loving climbers in locations near and dear to all of our hearts.</div>
+                    </div>
+                </div>
             </div>
             <div className='sub-container-three'>
-                <div>container 4</div>
+                <div>Find your next adventure</div>
+                <div>
+                    <div id='sub-tile-four'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                    <div id='sub-tile-five'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                    <div id='sub-tile-six'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                    <div id='sub-tile-seven'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                    <div id='sub-tile-eight'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                    <div id='sub-tile-nine'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                    <div id='sub-tile-ten'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                    <div id='sub-tile-eleven'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                    <div id='sub-tile-twelve'>
+                        <img />
+                        <div>Text</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
